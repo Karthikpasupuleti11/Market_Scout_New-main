08f424d7c13b1154355fb8005bf6688f8a5d8fca
Karthikpasupuleti11 <karthikpasupuletip@gmail.com>
2026-05-23 17:34:59 +0530

    feat: initialize frontend scaffolding with core navigation, API integration, and system status monitoring.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import * as Sentry from '@sentry/react'
+import Clarity from '@microsoft/clarity'
+
+Sentry.init({
+  dsn: "https://627ab330e91f7fe21f6800a3f679d0d1@o4511439056338944.ingest.de.sentry.io/4511439067611216",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+})
+
+Clarity.init("wvkr93m76x")
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
